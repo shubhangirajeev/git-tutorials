34cbb64d56a17de0720733c5533a5450252959e9
shubhangirajeev <shubhangirajeev7@gmail.com>
2021-11-11 20:07:49 +0530

    modify master

--- master.py
+++ master.py
@@ -1,2 +1,13 @@
-print('hello github')
-print(int(2.0))
+import os 
+
+cwd = os.getcwd()
+
+
+os.chdir(cwd+'/pictures')
+
+cwd = os.getcwd()
+
+file_list = os.listdir(cwd)
+
+for i in range(len(file_list)):
+    print(file_list[i])
